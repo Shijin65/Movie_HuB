642ef7c2b839947c89cc6b3048cf2cbcf46fe6da
shijin p <shijincht65@gmail.com>
2023-09-30 23:29:27 +0530

    pages folder added for creating the route pages

---
+++ client/src/Pages/Entrance.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../Components/Banner/Banner'
+import Cards from "../Components/Cards/Cards";
+import { originals } from "../url";
+function Entrance() {
+  return (
+    <div>
+        <Banner/>
+        <Cards Url={originals}  title="Shitflix"  />
+    </div>
+  )
+}
+
+export default Entrance
