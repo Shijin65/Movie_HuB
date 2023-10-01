448830d4fd6cd1b08c6da3d4ccd3c10723a2b70b
shijin p <shijincht65@gmail.com>
2023-10-02 00:48:03 +0530

    added toast message to the register user

--- client/src/Context/Authcontext.jsx
+++ client/src/Context/Authcontext.jsx
@@ -1,14 +1,18 @@
 import { createContext, useContext } from "react";
 import ToastContext from "./Toastcontext";
-
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export const AuthContextprovider = ({ children }) => {
 
-  const {Toast}=useContext(ToastContext)
+  const Navigate = useNavigate();
+  const { toast } = useContext(ToastContext);
+
+  //REGISTER USER ........................................................
+
   const RegisterUser = async (userData) => {
-    console.log(userData)
+    console.log(userData);
 
     try {
       const res = await fetch("http://localhost:8001/api/user/register", {
@@ -17,26 +21,52 @@ export const AuthContextprovider = ({ children }) => {
           "content-type": "application/json",
         },
         body: JSON.stringify(userData),
-      })
+      });
 
-      const userres = await res.json()
+      const userres = await res.json();
 
       if (!userres.error) {
         console.log("everything is fine");
-        Toast.success("registeration success")
+        toast.success("registeration success");
+        Navigate("/login", { replace: true });
       } else {
-        console.log(userres.error)
+        console.log(userres.error);
+        toast.error(userres.error);
       }
-
-
     } catch (error) {
+      toast.error(error);
+    }
+  };
 
+  //LOGIN USER ........................................................
+
+  const LoginUser = async(userData) => {
+
+    console.log(userData)
+    try {
+      // const res = await fetch("http://localhost:8001/api/user/login", {
+      //   method: "POST",
+      //   headers: {
+      //     "content-type": "application/json",
+      //   },
+      //   body: JSON.stringify(userData),
+      // });
+
+      // const userres = await res.json();
+    } catch (error) {
+      
     }
   }
+
+
+
+
+  //CURRENT USER ........................................................
+
   return (
-    <AuthContext.Provider value={{ RegisterUser }}>
+    <AuthContext.Provider value={{ RegisterUser ,LoginUser }}>
       {children}
     </AuthContext.Provider>
   );
 };
-export default AuthContext
+export default AuthContext;
