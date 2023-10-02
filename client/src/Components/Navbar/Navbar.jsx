b22abfc4ad988b9a950bad06b342247a231975f6
shijin p <shijincht65@gmail.com>
2023-10-02 11:24:28 +0530

    add link form react router dom to the navbar buttons to avoiding the refresh

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-// import './Navbar.css'
-
-
+import { Link } from "react-router-dom";
 
 function Navbar() {
  
@@ -12,8 +10,8 @@ function Navbar() {
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-2  text-white text-base">
-          <li><a href='/login'>Login</a></li>
-          <li><a href='/register'>Register</a></li>
+          <li><Link to={"/login"} >Login</Link></li>
+          <li><Link to={'/register'}>Register</Link></li>
         </ul>
         <div class="avatar placeholder">
           <div class="bg-slate-500  text-white rounded-full w-12">
--- client/src/Pages/Entrance.jsx
+++ client/src/Pages/Entrance.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Banner from '../Components/Banner/Banner'
-import Cards from "../Components/Cards/Cards";
-import { originals } from "../url";
+// import Cards from "../Components/Cards/Cards";
+// import { originals } from "../url";
 function Entrance() {
   return (
     <div>
         <Banner/>
-        <Cards Url={originals}  title="Shitflix"  />
+        {/* <Cards Url={originals}  title="Shitflix"  /> */}
     </div>
   )
 }
